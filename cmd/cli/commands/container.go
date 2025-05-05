4e21eb5a89283e34a8270d88c6f951becef72e81
ajesicus <ajesicus.dev@outlook.com>
2025-05-06 00:13:31 +0700

    update subcommands of container

--- cmd/cli/commands/container.go
+++ cmd/cli/commands/container.go
@@ -8,8 +8,9 @@ import (
 )
 
 var (
-	upFlag   bool
-	downFlag bool
+	upFlag    bool
+	downFlag  bool
+	vaultFlag bool
 )
 
 var containerCmd = &cobra.Command{
@@ -35,7 +36,51 @@ var containerCmd = &cobra.Command{
 	},
 }
 
+var vaultComposeFiles = []string{
+	"-f", "./deployments/vault/docker-compose.yml",
+	"-f", "./deployments/vault/docker-compose.dev.yml",
+}
+
+var upCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Start containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Starting containers...")
+
+		dockerArgs := []string{"compose"}
+		if vaultFlag {
+			dockerArgs = append(dockerArgs, vaultComposeFiles...)
+		}
+
+		dockerArgs = append(dockerArgs, "up", "-d")
+
+		run("docker", dockerArgs...)
+	},
+}
+
+var downCmd = &cobra.Command{
+	Use:   "down",
+	Short: "Stop containers",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Stopping containers...")
+		dockerArgs := []string{"compose"}
+		if vaultFlag {
+			dockerArgs = append(dockerArgs, vaultComposeFiles...)
+		}
+
+		dockerArgs = append(dockerArgs, "down")
+
+		run("docker", dockerArgs...)
+	},
+}
+
 func init() {
+	containerCmd.AddCommand(upCmd)
+	upCmd.Flags().BoolVar(&vaultFlag, "vault", false, "Start only containers of vault")
+
+	containerCmd.AddCommand(downCmd)
+	downCmd.Flags().BoolVar(&vaultFlag, "vault", false, "Stop only containers of vault")
+
 	rootCmd.AddCommand(containerCmd)
 	containerCmd.Flags().BoolVar(&upFlag, "up", false, "Start the container")
 	containerCmd.Flags().BoolVar(&downFlag, "down", false, "Stop the container")
